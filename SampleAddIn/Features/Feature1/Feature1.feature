56cfd43e08090081442f6bd0b4f5f131b9a437b1
Mikael Salmi <mikael@salmiconsulting.eu>
2016-10-17 21:33:07 +0200

    First commit

---
+++ SampleAddIn/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d0ed51cf-3a7e-44ee-aa6f-31215b02f68d" featureId="d0ed51cf-3a7e-44ee-aa6f-31215b02f68d" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SampleAddIn Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f4402337-e3b0-448e-8b8a-b2258bf7c230" />
+  </projectItems>
+</feature>
--- SampleAddInWeb/Pages/Default.aspx.cs
+++ SampleAddInWeb/Pages/Default.aspx.cs
@@ -5,10 +5,24 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Microsoft.SharePoint.Client;
+using Microsoft.IdentityModel.S2S.Tokens;
+using System.Net;
+using System.IO;
+using System.Xml;
+
 namespace SampleAddInWeb
 {
     public partial class Default : System.Web.UI.Page
     {
+        SharePointContextToken contextToken;
+        string accessToken;
+        Uri sharepointUrl;
+        string siteName;
+        string currentUser;
+        List<string> listOfUsers = new List<string>();
+        List<string> listOfLists = new List<string>();
+
         protected void Page_PreInit(object sender, EventArgs e)
         {
             Uri redirectUrl;
@@ -26,17 +40,87 @@ namespace SampleAddInWeb
             }
         }
 
+        protected void CSOM_Click(object sender, EventArgs e)
+        {
+            string commandAccessToken = ((LinkButton)sender).CommandArgument;
+            RetrieveWithCSOM(commandAccessToken);
+            WebTitleLabel.Text = siteName;
+            CurrentUserLabel.Text = currentUser;
+            UserList.DataSource = listOfUsers;
+            UserList.DataBind();
+            ListList.DataSource = listOfLists;
+            ListList.DataBind();
+        }
+
+        // This method retrieves information about the host web by using the CSOM.
+        private void RetrieveWithCSOM(string accessToken)
+        {
+
+            if (IsPostBack)
+            {
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+            }
+
+
+            ClientContext clientContext =
+                    TokenHelper.GetClientContextWithAccessToken(
+                        sharepointUrl.ToString(), accessToken);
+
+
+            // Load the properties for the web object.
+            Web web = clientContext.Web;
+            clientContext.Load(web);
+            clientContext.ExecuteQuery();
+
+            // Get the site name.
+            siteName = web.Title;
+
+            // Get the current user.
+            clientContext.Load(web.CurrentUser);
+            clientContext.ExecuteQuery();
+            currentUser = clientContext.Web.CurrentUser.LoginName;
+
+            // Load the lists from the Web object.
+            ListCollection lists = web.Lists;
+            clientContext.Load<ListCollection>(lists);
+            clientContext.ExecuteQuery();
+
+            // Load the current users from the Web object.
+            UserCollection users = web.SiteUsers;
+            clientContext.Load<UserCollection>(users);
+            clientContext.ExecuteQuery();
+
+            foreach (User siteUser in users)
+            {
+                listOfUsers.Add(siteUser.LoginName);
+            }
+
+            foreach (List list in lists)
+            {
+                listOfLists.Add(list.Title);
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            // The following code gets the client context and Title property by using TokenHelper.
-            // To access other properties, the app may need to request permissions on the host web.
-            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+            string contextTokenString =
+                 TokenHelper.GetContextTokenFromRequest(Request);
 
-            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            if (contextTokenString != null)
             {
-                clientContext.Load(clientContext.Web, web => web.Title);
-                clientContext.ExecuteQuery();
-                Response.Write(clientContext.Web.Title);
+                contextToken =
+                    TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                accessToken =
+                    TokenHelper.GetAccessToken(contextToken, sharepointUrl.Authority)
+                               .AccessToken;
+                CSOM.CommandArgument = accessToken;
+            }
+            else if (!IsPostBack)
+            {
+                Response.Write("Could not find a context token.");
+                return;
             }
         }
     }
